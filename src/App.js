fe93952f2ae4b5853894fd450fbb9ec522294eae
unknown <daviddimas80@gmail.com>
2022-07-31 00:33:07 +0700

    login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
 
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import Login from './components/login'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-     
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Router>
+      <Routes>
+           <Route exact path="/" element={<Login/>}/>
+             
+             
+           </Routes>
+  </Router>
   );
 }
 
---
+++ src/components/login.js
@@ -0,0 +1,74 @@
+import React, {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import axios from "axios";
+import "bulma/css/bulma.min.css";
+
+const Login = () => {
+
+  const [username, setUserName] = useState();
+  const [password, setPassword] = useState();
+
+  /* TOKEN */
+
+
+  const history = useNavigate();
+
+  /* Check table user di db */
+  const login = async (e) => {
+    
+    if(username && password) {
+      
+      const devEnv = process.env.NODE_ENV !== "production";
+      const {REACT_APP_DEV_URL, REACT_APP_PROD_URL} = process.env;
+  
+      await axios.get(`${devEnv ? REACT_APP_DEV_URL : REACT_APP_PROD_URL}/user`, {
+        params: {
+          username:username,
+          password:password
+        }
+      }).then((respon) => {
+
+        if(respon.data.length !== 0) {
+          history.push("/home");
+        }
+        else{
+        
+          window.alert("Invalid username or password");
+        }
+
+      }).catch((err) => console.log(err));
+
+    }
+    else{
+      window.alert("Username or password can't be empty");
+    }
+
+  }
+  /*  */
+  return (
+
+  <center>
+
+    <div className="loginContainer">
+      
+      <div className="loginTitle">Log In</div>
+      
+      <div className="loginLabel">Username</div>
+      <div className="loginInput"><input type="text" onChange={e => setUserName(e.target.value)} spellCheck="false" required/></div>
+
+      <div className="loginLabel">Password</div>
+      <div className="loginInput"><input type="password" onChange={e => setPassword(e.target.value)} required/></div>
+
+      <button className="loginButton" onClick={login}>Login</button>
+      
+      <a href="/signup" className="linkButton"><button className="signupButton">Sign Up</button></a>
+    
+    </div>
+
+  </center>
+
+  )
+
+}
+
+export default Login;
