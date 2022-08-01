03e7c4334f1e599a50c84c0d39c0288508dfbf8b
unknown <daviddimas80@gmail.com>
2022-08-01 23:02:11 +0700

    fix deploy

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 dbm.connect()
-
+app.use(express.static('public'))
 require('dotenv').config()
 
 app.get('/user',function (req,res){
