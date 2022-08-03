095738a3b6400a43e5654fcb8f0ddfdf54a89e72
unknown <daviddimas80@gmail.com>
2022-08-03 21:18:11 +0700

    test deploy

--- server.js
+++ server.js
@@ -57,9 +57,12 @@ app.patch('/updategereja',function(req,res){
     dbm.deleteUser(req.body.id)
   })
 
-  app.use(express.static('public'));
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public', 'index.html'));
-  });
+  if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'build')));
+    app.get('/*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  
+    });
+  }
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
