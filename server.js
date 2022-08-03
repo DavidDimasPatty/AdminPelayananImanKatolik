40d7165da4429405de8b8bc32a3c8a40a5a82269
unknown <daviddimas80@gmail.com>
2022-08-03 21:30:30 +0700

    bug production

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const dbm=require('./db')
 const db = require('./db')
 const bodyParser = require("body-parser");
+const { Router } = require('express');
 const PORT = process.env.PORT || 5000
 const app = express()
 
@@ -58,11 +59,11 @@ app.patch('/updategereja',function(req,res){
   })
 
   if(process.env.NODE_ENV === 'production') {
-    app.use('/api', jsonServer.router('./db.js')) 
+    app.use('/api', Router.call('./db.js')) 
     app.use(express.static(path.join(__dirname, 'build')));
     app.get('/*', function(req, res) {
       res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  
+        
     });
   }
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
