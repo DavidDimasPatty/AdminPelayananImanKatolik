a3617590a84ecaa17829668ddc7c50666a9fd1de
unknown <daviddimas80@gmail.com>
2022-08-03 23:46:09 +0700

    url deploy

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.patch('/updategereja',function(req,res){
           
       });
     app.use(express.static(path.join(__dirname, 'build')));
-    app.get('/*', function(req, res) {
+    app.get('/', function(req, res) {
       res.sendFile(path.join(__dirname, 'build', 'index.html'));
         
     });
