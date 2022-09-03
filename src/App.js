89f27138f264f3e3bc6c1868ba5e9eaaea700b35
unknown <daviddimas80@gmail.com>
2022-09-04 02:14:05 +0700

    forgot password page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Login from './components/login'
 import Home from './components/home'
 import EditGereja from "./components/editGereja";
 import AddGereja from "./components/addGereja";
+import ChangePassword from "./components/changePassword";
 
 function App() {
   return (
@@ -17,7 +18,7 @@ function App() {
            <Route exact path="/daftaruser" element={<DaftarUser/>}/>
            <Route exact path="/editgereja/:id" element={<EditGereja/>}/>
            <Route exact path="/pageaddgereja" element={<AddGereja/>}/>
-             
+           <Route exact path="/pageForgotPassword/:email" element={<ChangePassword/>}/>
              
            </Routes>
   </Router>
